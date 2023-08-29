f77830910505ce0bb49ea7b65245305e051916d8
Thomas Kugi <thomas.kugi@syon.at>
2023-08-29 16:55:59 +0200

    added try catch with jsonparse to allow the usage of mysql and mariadb

--- src/utils/getLastMigrationState.ts
+++ src/utils/getLastMigrationState.ts
@@ -1,22 +1,37 @@
-import { Sequelize } from "sequelize-typescript";
+import {Sequelize} from "sequelize-typescript";
+
 export default async function getLastMigrationState(sequelize: Sequelize) {
-  const [
-    lastExecutedMigration,
-  ] = await sequelize.query(
-    "SELECT name FROM SequelizeMeta ORDER BY name desc limit 1",
-    { type: "SELECT" }
-  );
+    const [
+        lastExecutedMigration,
+    ] = await sequelize.query(
+        "SELECT name FROM SequelizeMeta ORDER BY name desc limit 1",
+        {type: "SELECT"}
+    );
 
-  const lastRevision: number =
-    lastExecutedMigration !== undefined
-      ? parseInt(lastExecutedMigration["name"].split("-")[0])
-      : -1;
+    const lastRevision: number =
+        lastExecutedMigration !== undefined
+            ? parseInt(lastExecutedMigration["name"].split("-")[0])
+            : -1;
 
-  const [
-    lastMigration,
-  ] = await sequelize.query(
-    `SELECT state FROM SequelizeMetaMigrations where revision = '${lastRevision}'`,
-    { type: "SELECT" }
-  );
-  return lastMigration ? lastMigration["state"] : undefined;
+    const [
+        lastMigration,
+    ] = await sequelize.query(
+        `SELECT state
+         FROM SequelizeMetaMigrations
+         where revision = '${lastRevision}'`,
+        {type: "SELECT"}
+    );
+
+    if (!lastMigration) {
+        return undefined
+    }
+
+    try {
+        // mariadb need a JSON parse
+        return JSON.parse(lastMigration["state"])
+    } catch (e) {
+        // mysql does not
+        return lastMigration["state"]
+    }
+
 }
